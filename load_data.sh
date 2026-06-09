07cb26bc9f87680bb35c4c270fefe3c0b7babfd8
jboussouf <jamalbssouf@gmail.com>
2026-06-09 16:03:54 +0100

    add data download script

---
+++ load_data.sh
@@ -0,0 +1,2 @@
+echo "#################################  CIRCL Forensics Exercises  ###################################"
+wget https://digitalcorpora.s3.amazonaws.com/corpora/drives/circl-2023-wiped/wiped_disk.E01 -d ./vf_datasets
